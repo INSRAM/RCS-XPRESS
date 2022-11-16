ecd48fdbfb922cae8fe7143788fd8a1b7b599def
INSRAM <insram786@gmail.com>
2022-11-17 02:03:37 +0500

    front end almost completed

---
+++ src/compnents/snackbar/Snackbar.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+
+export default function SimpleSnackbar(props) {
+  const [open, setOpen] = React.useState(props.open);
+
+  const handleClose = (event, reason) => {
+    setOpen(false);
+  };
+  //   React.useEffect(() => {
+  //     setOpen(props.open);
+  //   }, [props.open]);
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  return (
+    <div>
+      {/* <Button onClick={handleClick}>Open simple snackbar</Button> */}
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={props.message}
+        action={action}
+      />
+    </div>
+  );
+}
---
+++ src/pages/maps/Maps.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Maps() {
+  return (
+    <div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d435519.9878498228!2d74.3343893!3d31.4829403!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39190483e58107d9%3A0xc23abe6ccc7e2462!2sLahore%2C%20Punjab!5e0!3m2!1sen!2s!4v1668615020437!5m2!1sen!2s"
+        width="100%"
+        height="300px"
+        style={{ border: 0 }}
+        loading="lazy"
+      />
+    </div>
+  );
+}
+
+export default Maps;
