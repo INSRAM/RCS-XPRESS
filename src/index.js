dca1b47679eb9887c50152365595014dd8741228
INSRAM <insram786@gmail.com>
2022-12-24 19:31:52 +0500

    rcs front-end

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CookiesProvider } from "react-cookie";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <CookiesProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </CookiesProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,17 +1,8 @@
 import React from "react";
-import { useCookies } from "react-cookie";
 import Login from "../AdminLogin/AdminLogin";
 import AdminSection from "../../compnents/AdminSection/AdminSection";
 
 function Admin() {
-  // const [cookies, setCookie] = useCookies(["user"]);
-  // if (cookies.user === undefined) {
-  //   return (
-  //     <>
-  //       <Login />
-  //     </>
-  //   );
-  // }
   return <AdminSection />;
 }
 
